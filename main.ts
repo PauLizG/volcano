2520d1e3e6582ff54d2786ffdb38975702159617
PauLizG <112582743+PauLizG@users.noreply.github.com>
2022-09-13 10:48:32 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,76 @@
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+}
